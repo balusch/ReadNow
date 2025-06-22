0efa3403b4fd91378b00e5590ea9b50bd36f1fe6
Jianyong Chen <baluschch@gmail.com>
2025-06-22 12:00:45 +0100

    Remember window size and fix icon.
    Signed-off-by: Jianyong Chen <baluschch@gmail.com>

--- main.js
+++ main.js
@@ -1,21 +1,76 @@
 // main.js
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray, Menu, globalShortcut } = require('electron')
+const path = require('path')
+const { default: Store } = require('electron-store')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+const store = new Store()
+
+let win
+let tray
+
+function createWindow() {
+  const bounds = store.get('windowBounds') || { width: 1200, height: 800 }
+
+  win = new BrowserWindow({
+    width: bounds.width,
+    height: bounds.height,
+    x: bounds.x,
+    y: bounds.y,
     webPreferences: {
-      nodeIntegration: false
-    }
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+    },
+    title: 'ReadNow',
+    icon: path.join(__dirname, 'assets', 'icon.png')
   })
 
   win.loadURL('https://weread.qq.com/')
+
+  win.on('close', () => {
+    store.set('windowBounds', win.getBounds())
+  })
+
+  // Optional: when minimized, hide to tray instead of closing
+  // win.on('minimize', (event) => {
+  //   event.preventDefault()
+  //   win.hide()
+  // })
 }
 
-app.whenReady().then(createWindow)
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'assets', 'icon.png'))
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show', click: () => win.show() },
+    { label: 'Quit', click: () => app.quit() },
+  ])
+  tray.setToolTip('ReadNow')
+  tray.setContextMenu(contextMenu)
+}
+
+function registerShortcuts() {
+  globalShortcut.register('CommandOrControl+R', () => {
+    if (win) win.reload()
+  })
+  globalShortcut.register('CommandOrControl+Shift+I', () => {
+    if (win) win.webContents.openDevTools()
+  })
+  globalShortcut.register('Escape', () => {
+    if (win) win.minimize()
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  createTray()
+  registerShortcuts()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
